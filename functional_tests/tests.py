2ced11d12ddc3315dd648bcde5323f12947924fd
Matt Chan <thewchan@gmail.com>
2021-04-23 17:01:35 -0400

    First steps of FT for layout + styleing

--- functional_tests/tests.py
+++ functional_tests/tests.py
@@ -123,6 +123,30 @@ class NewVisitorTest(LiveServerTestCase):
         self.assertNotIn("Buy peacock feathers", page_text)
         self.assertIn("Buy milk", page_text)
 
+    def test_layout_and_styling(self) -> None:
+        """Test the aesthetic implementation of the site."""
+        # Edith goes to the home page
+        self.browser.get(self.live_server_url)
+        self.browser.set_window_size(1024, 768)
+
+        # She notices the input box is nicely centered
+        inputbox = self.browser.find_element_by_id("id_new_item")
+        self.assertAlmostEqual(
+            inputbox.location["x"] + inputbox.size["width"] / 2,
+            512,
+            delta=10,
+        )
+
+        # She starts a new list and sees the input is nicely centered
+        inputbox.send_keys("testing")
+        inputbox.send_keys(Keys.ENTER)
+        self.wait_for_row_in_list_table("1: testing")
+        inputbox = self.browser.find_element_by_id("id_new_item")
+        self.assertAlmostEqual(
+            inputbox.location["x"] + inputbox.size["width"] / 2,
+            512,
+            delta=10,
+        )
 # She visits that URL - her to-do list is still there
 
 # Satisfied, she goes back to sleep
