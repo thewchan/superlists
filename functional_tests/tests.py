2fa92a6fca6f0cf9dab47d510503d4f704556170
Matt Chan <thewchan@gmail.com>
2021-04-23 23:25:40 -0400

    Use bootstrap to improve layout

--- functional_tests/tests.py
+++ functional_tests/tests.py
@@ -1,7 +1,7 @@
 """Function tests for the SuperList webapp."""
 import time
 
-from django.test import LiveServerTestCase
+from django.contrib.staticfiles.testing import StaticLiveServerTestCase
 from selenium import webdriver
 from selenium.common.exceptions import WebDriverException
 from selenium.webdriver.common.keys import Keys
@@ -10,7 +10,7 @@ from selenium.webdriver.common.keys import Keys
 MAX_WAIT = 10
 
 
-class NewVisitorTest(LiveServerTestCase):
+class NewVisitorTest(StaticLiveServerTestCase):
     """Test class related to new visitor to superlist."""
 
     def setUp(self) -> None:
