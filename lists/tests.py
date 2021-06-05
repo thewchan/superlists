bd6e480c3fc85f8f2e975517f8bb2c3165d6dda0
Matt Chan <thewchan@gmail.com>
2021-06-05 14:40:04 -0400

    Added more functional tests

--- lists/tests.py
+++ lists/tests.py
@@ -10,6 +10,12 @@ class HomePageTest(TestCase):
         response = self.client.get("/")
         self.assertTemplateUsed(response, "home.html")
 
+    def test_can_save_a_POST_request(self) -> None:  # noqa: N802
+        """Test that a POST request can be saved."""
+        response = self.client.post("/", data={"item_text": "A new list item"})
+        self.assertIn("A new list item", response.content.decode())
+        self.assertTemplateUsed(response, "home.html")
+
     def test_home_page_returns_correct_html(self) -> None:
         """Test the home page links to correct html code."""
         response = self.client.get("/")
