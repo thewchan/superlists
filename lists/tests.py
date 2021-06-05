67c76a017e9b078d42c526df2bb85c8811863e88
Matt Chan <thewchan@gmail.com>
2021-06-05 15:10:58 -0400

    Redirect after POST, and show all items in template.

--- lists/tests.py
+++ lists/tests.py
@@ -14,8 +14,16 @@ class HomePageTest(TestCase):
     def test_can_save_a_POST_request(self) -> None:  # noqa: N802
         """Test that a POST request can be saved."""
         response = self.client.post("/", data={"item_text": "A new list item"})
-        self.assertIn("A new list item", response.content.decode())
-        self.assertTemplateUsed(response, "home.html")
+
+        self.assertEqual(Item.objects.count(), 1)
+        new_item = Item.objects.first()
+        self.assertEqual(new_item.text, "A new list item")
+
+    def test_redirects_after_POST(self) -> None:  # noqa: N802
+        """Test redirection after a POST request"""
+        response = self.client.post("/", data={"item_text": "A new list item"})
+        self.assertEqual(response.status_code, 302)
+        self.assertEqual(response["location"], "/")
 
     def test_home_page_returns_correct_html(self) -> None:
         """Test the home page links to correct html code."""
@@ -26,11 +34,26 @@ class HomePageTest(TestCase):
         self.assertTrue(html.strip().endswith("</html>"))
         self.assertTemplateUsed(response, "home.html")
 
+    def test_only_saves_items_when_necessary(self) -> None:
+        """Test to-do list items are saved appropriately."""
+        self.client.get("/")
+        self.assertEqual(Item.objects.count(), 0)
+
+    def test_displays_all_list_items(self) -> None:
+        """Test that multiple list items are rendered."""
+        Item.objects.create(text="itemey 1")
+        Item.objects.create(text="itemey 2")
+
+        response = self.client.get("/")
+
+        self.assertIn("itemey 1", response.content.decode())
+        self.assertIn("itemey 2", response.content.decode())
+
 
 class ItemModelTest(TestCase):
     """Test suite for Django models"""
 
-    def test_saving_and_retrieving_items(self):
+    def test_saving_and_retrieving_items(self) -> None:
         """Test database transactions."""
         first_item = Item()
         first_item.text = "The first (ever) list item"
--- lists/views.py
+++ lists/views.py
@@ -1,14 +1,15 @@
 """Django views for to-do list app."""
 from django.http import HttpRequest, HttpResponse
-from django.shortcuts import render
+from django.shortcuts import redirect, render
+from lists.models import Item
 
 
 def home_page(request: HttpRequest) -> HttpResponse:
     """Renders home page."""
-    return render(
-        request,
-        "home.html",
-        {
-            "new_item_text": request.POST.get("item_text", ""),
-        },
-    )
+    if request.method == "POST":
+        Item.objects.create(text=request.POST["item_text"])
+        return redirect("/")
+
+    items = Item.objects.all()
+
+    return render(request, "home.html", {"items": items})
