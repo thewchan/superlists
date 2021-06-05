81096022516d2f47762e7016cf1ba5a498c6499f
Matt Chan <thewchan@gmail.com>
2021-06-05 18:04:26 -0400

    First steps of FT for layout + styling

--- functional_tests/tests.py
+++ functional_tests/tests.py
@@ -20,6 +20,28 @@ class NewVisitorTest(LiveServerTestCase):
         """Quit browser."""
         self.browser.quit()
 
+    def test_layout_and_styling(self) -> None:
+        """Test for correct layout rendering."""
+        # Edith goes to the home page
+        self.browser.get(self.live_server_url)
+        self.browser.set_window_size(1024, 768)
+
+        # She notices the input box is nicely centered
+        inputbox = self.browser.find_element_by_id("id_new_item")
+        self.assertAlmostEqual(
+            inputbox.location["x"] + inputbox.size["width"] / 2, 512, delta=10
+        )
+
+        # She starts a new list and sees the input is nicely
+        # centered there too
+        inputbox.send_keys("testing")
+        inputbox.send_keys(Keys.ENTER)
+        self.wait_for_row_in_list_table("1: testing")
+        inputbox = self.browser.find_element_by_id("id_new_item")
+        self.assertAlmostEqual(
+            inputbox.location["x"] + inputbox.size["width"] / 2, 512, delta=10
+        )
+
     def wait_for_row_in_list_table(self, row_text) -> None:
         """Test that a new row is added to to-do list, helper method."""
         start_time = time.time()
