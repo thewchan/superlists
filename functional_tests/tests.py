8b1d0ec81af11b95b946c42c365ee02863f98d49
Matt Chan <thewchan@gmail.com>
2021-06-05 18:40:21 -0400

    Use Bootstrap to improve layout

--- functional_tests/tests.py
+++ functional_tests/tests.py
@@ -1,6 +1,6 @@
 """Functional tests for TDD book."""
 import time
-from django.test import LiveServerTestCase
+from django.contrib.staticfiles.testing import StaticLiveServerTestCase
 from selenium import webdriver
 from selenium.common.exceptions import WebDriverException
 from selenium.webdriver.common.keys import Keys
@@ -9,7 +9,7 @@ from selenium.webdriver.common.keys import Keys
 MAX_WAIT = 10
 
 
-class NewVisitorTest(LiveServerTestCase):
+class NewVisitorTest(StaticLiveServerTestCase):
     """Test for new visitor to app."""
 
     def setUp(self) -> None:
