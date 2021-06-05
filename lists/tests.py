bc03b46de2f61358d68b18969128be6d02e132b4
Matt Chan <thewchan@gmail.com>
2021-06-05 15:46:09 -0400

    Added functional tests for multiple users

--- lists/tests.py
+++ lists/tests.py
@@ -13,14 +13,14 @@ class HomePageTest(TestCase):
 
     def test_can_save_a_POST_request(self) -> None:  # noqa: N802
         """Test that a POST request can be saved."""
-        response = self.client.post("/", data={"item_text": "A new list item"})
+        self.client.post("/", data={"item_text": "A new list item"})
 
         self.assertEqual(Item.objects.count(), 1)
         new_item = Item.objects.first()
         self.assertEqual(new_item.text, "A new list item")
 
     def test_redirects_after_POST(self) -> None:  # noqa: N802
-        """Test redirection after a POST request"""
+        """Test redirection after a POST request."""
         response = self.client.post("/", data={"item_text": "A new list item"})
         self.assertEqual(response.status_code, 302)
         self.assertEqual(response["location"], "/")
