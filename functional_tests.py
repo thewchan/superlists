1da95211a46fdbe85a8390589108954fb182139c
Matt Chan <thewchan@gmail.com>
2021-06-05 14:46:09 -0400

    Added helper method to functional tests

--- functional_tests.py
+++ functional_tests.py
@@ -16,6 +16,12 @@ class NewVisitorTest(unittest.TestCase):
         """Quit browser."""
         self.browser.quit()
 
+    def check_for_row_in_list_table(self, row_text) -> None:
+        """Test that a new row is added to to-do list, helper method."""
+        table = self.browser.find_element_by_id("id_list_table")
+        rows = table.find_elements_by_tag_name("tr")
+        self.assertIn(row_text, [row.text for row in rows])
+
     def test_can_start_a_list_and_retrieve_it_later(self) -> None:
         """Test for starting a to-do list and its retrieval."""
         # Edith has heard about a cool new online to-do app. She goes
@@ -41,10 +47,7 @@ class NewVisitorTest(unittest.TestCase):
         # "1: Buy peacock feathers" as an item in a to-do list
         inputbox.send_keys(Keys.ENTER)
         time.sleep(1)
-
-        table = self.browser.find_element_by_id("id_list_table")
-        rows = table.find_elements_by_tag_name("tr")
-        self.assertIn("1: Buy peacock feathers", [row.text for row in rows])
+        self.check_for_row_in_list_table("1: Buy peacock feathers")
 
         # There is still a text box inviting her to add another item. She
         # enters "Use peacock feathers to make a fly"(Edith is very methodical)
@@ -54,11 +57,9 @@ class NewVisitorTest(unittest.TestCase):
         time.sleep(1)
 
         # The page updates again, and now shows both items on her list
-        table = self.browser.find_element_by_id("id_list_table")
-        rows = table.find_elements_by_tag_name("tr")
-        self.assertIn("1: Buy peacock feathers", [row.text for row in rows])
-        self.assertIn(
-            "2: Use peacock feathers to make a fly", [row.text for row in rows]
+        self.check_for_row_in_list_table("1: Buy peacock feathers")
+        self.check_for_row_in_list_table(
+            "2: Use peacock feathers to make a fly"
         )
 
         # Edith wonders whether the site will remember her list. Then she sees
