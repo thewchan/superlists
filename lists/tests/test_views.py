8c54188d052aca0bb0b8f6d92ca3354cd24918d7
Matt Chan <thewchan@gmail.com>
2021-06-05 23:45:16 -0400

    Split out unit tests into 2 files

--- lists/tests/test_views.py
+++ lists/tests/test_views.py
@@ -1,4 +1,4 @@
-"""Test suite for list app."""
+"""Test suite for views unit tests."""
 from django.test import TestCase
 from lists.models import Item, List
 
@@ -10,47 +10,6 @@ class HomePageTest(TestCase):
         """Test for homepage rendering."""
         response = self.client.get("/")
         self.assertTemplateUsed(response, "home.html")
-
-    def test_home_page_returns_correct_html(self) -> None:
-        """Test the home page links to correct html code."""
-        response = self.client.get("/")
-        html = response.content.decode("utf8")
-        self.assertTrue(html.startswith("<html>"))
-        self.assertIn("<title>To-Do lists</title>", html)
-        self.assertTrue(html.strip().endswith("</html>"))
-        self.assertTemplateUsed(response, "home.html")
-
-
-class ListAndItemModelsTest(TestCase):
-    """Test suite for Django models."""
-
-    def test_saving_and_retrieving_items(self) -> None:
-        """Test database transactions."""
-        list_ = List()
-        list_.save()
-
-        first_item = Item()
-        first_item.text = "The first (ever) list item"
-        first_item.list = list_
-        first_item.save()
-
-        second_item = Item()
-        second_item.text = "Item the second"
-        second_item.list = list_
-        second_item.save()
-
-        saved_list = List.objects.first()
-        self.assertEqual(saved_list, list_)
-
-        saved_items = Item.objects.all()
-        self.assertEqual(saved_items.count(), 2)
-
-        first_saved_item = saved_items[0]
-        second_saved_item = saved_items[1]
-        self.assertEqual(first_saved_item.text, "The first (ever) list item")
-        self.assertEqual(first_saved_item.list, list_)
-        self.assertEqual(second_saved_item.text, "Item the second")
-        self.assertEqual(second_saved_item.list, list_)
 
 
 class ListViewTest(TestCase):
